52be476c43c8b9e848fb3c4b67b41c3bfbb694a7
Will Fox <WEFox@student.fullsail.edu>
2022-05-09 16:18:10 -0400

    Calculator layout finished. Now I need a sizer.

--- SoftwareEngineeringCalculator/MainWindow.cpp
+++ SoftwareEngineeringCalculator/MainWindow.cpp
@@ -2,10 +2,20 @@
 #include <vector>
 #include <string>
 
-MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), wxSize(300, 400)) {
+MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), wxSize(337, 480)) {
+	wxSize size = wxSize(80, 60);
+	int y = 80;
+
+	txt = new wxTextCtrl(this, 0, "", wxPoint(0, 0), wxSize(320, 80));
 	
+	for (int i = 1; i <= 24; ++i) {
+		buttons[i-1] = new wxButton(this, i, symbols[i-1], wxPoint(((i - 1) % 4) * 80, y), size);
+		if (i % 4 == 0) {
+			y += 60;
+		}
+	}
+
 }
 
 MainWindow::~MainWindow() {
-	
 }
--- SoftwareEngineeringCalculator/MainWindow.h
+++ SoftwareEngineeringCalculator/MainWindow.h
@@ -1,16 +1,42 @@
 #pragma once
 #include "wx/wx.h"
+#include <vector>
+#include <string>
 class MainWindow : public wxFrame
 {
 public:
 	MainWindow();
 	~MainWindow();
-
 public:
-	int nFieldWidth = 4;
-	int nFieldHeight = 6;
-	wxButton** btn;
-	int* nField = nullptr;
-	wxTextCtrl* txtCtrl = nullptr;
+	wxButton* mod = nullptr;
+	wxButton* CE = nullptr;
+	wxButton* C = nullptr;
+	wxButton* DEL = nullptr;
+	wxButton* dec = nullptr;
+	wxButton* bin = nullptr;
+	wxButton* hex = nullptr;
+	wxButton* div = nullptr;
+	wxButton* seven = nullptr;
+	wxButton* eight = nullptr;
+	wxButton* nine = nullptr;
+	wxButton* mult = nullptr;
+	wxButton* four = nullptr;
+	wxButton* five = nullptr;
+	wxButton* six = nullptr;
+	wxButton* min = nullptr;
+	wxButton* one = nullptr;
+	wxButton* two = nullptr;
+	wxButton* three = nullptr;
+	wxButton* plus = nullptr;
+	wxButton* neg = nullptr;
+	wxButton* zero = nullptr;
+	wxButton* dot = nullptr;
+	wxButton* equals = nullptr;
+
+	wxTextCtrl* txt = nullptr;
+
+	std::vector<wxButton*> buttons{ mod, CE, C, DEL, dec, bin, hex, div, seven, eight, nine, mult, four, five, six, min, one, two, three, plus, neg, zero, dot, equals };
+	std::vector<std::string> symbols{ "%", "CE", "C", "DEL", "dec.", "bin.", "hex.", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "-/+", "0", ".", "=" };
+
 };
 
