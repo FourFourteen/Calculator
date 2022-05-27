c24a13fcd4deb8daf99b684fe87b321effbfc518
Will Fox <WEFox@student.fullsail.edu>
2022-05-27 19:04:48 -0400

    Classes created for tests

---
+++ SoftwareEngineeringCalculatorTests/ButtonFactoryTests.cpp
@@ -0,0 +1,44 @@
+#include "ButtonFactoryTests.h"
+
+void ButtonFactoryTests::isID1() {
+	MainWindow* window = new MainWindow();
+	ButtonFactory* factory = new ButtonFactory(window);
+
+
+}
+
+void ButtonFactoryTests::isID5() {
+
+}
+
+void ButtonFactoryTests::isID10() {
+
+}
+
+void ButtonFactoryTests::isID15() {
+
+}
+
+void ButtonFactoryTests::isID20() {
+
+}
+
+void ButtonFactoryTests::isLabel414() {
+
+}
+
+void ButtonFactoryTests::isLabelBruh() {
+
+}
+
+void ButtonFactoryTests::isLabelBruh414() {
+
+}
+
+void ButtonFactoryTests::isLabelOK() {
+
+}
+
+void ButtonFactoryTests::isLabelWilliam() {
+
+}
---
+++ SoftwareEngineeringCalculatorTests/ButtonFactoryTests.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "wx/wx.h"
+#include "../SoftwareEngineeringCalculator/ButtonFactory.h"
+#include "../SoftwareEngineeringCalculator/MainWindow.h"
+
+class ButtonFactoryTests
+{
+	void isID1();
+	void isID5();
+	void isID10();
+	void isID15();
+	void isID20();
+
+	void isLabel414();
+	void isLabelBruh();
+	void isLabelBruh414();
+	void isLabelOK();
+	void isLabelWilliam();
+};
+
