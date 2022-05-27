c66f2ee3eb2d4669837cdcd4b6029af839689a70
Will Fox <WEFox@student.fullsail.edu>
2022-05-27 13:50:34 -0400

    Button factory changed

--- SoftwareEngineeringCalculator/ButtonFactory.h
+++ SoftwareEngineeringCalculator/ButtonFactory.h
@@ -10,10 +10,37 @@ private:
 	wxSize size = wxSize(80, 60);
 	MainWindow* win;
 public:
-	ButtonFactory(MainWindow* _win = nullptr);
+	ButtonFactory(MainWindow* _win);
 	wxButton* CreateButton();
 	int y = 80;
 	int curRow = 0;
 	int curCol = 0;
-};
 
+	wxButton* CreateButton1();
+	wxButton* CreateButton2();
+	wxButton* CreateButton3();
+	wxButton* CreateButton4();
+	wxButton* CreateButton5();
+	wxButton* CreateButton6();
+	wxButton* CreateButton7();
+	wxButton* CreateButton8();
+	wxButton* CreateButton9();
+	wxButton* CreateButton10();
+	wxButton* CreateButton11();
+	wxButton* CreateButton12();
+	wxButton* CreateButton13();
+	wxButton* CreateButton14();
+	wxButton* CreateButton15();
+	wxButton* CreateButton16();
+	wxButton* CreateButton17();
+	wxButton* CreateButton18();
+	wxButton* CreateButton19();
+	wxButton* CreateButton20();
+	wxButton* CreateButton21();
+	wxButton* CreateButton22();
+	wxButton* CreateButton23();
+	wxButton* CreateButton24();
+
+
+
+};
